5d1e629a8586843b41192caef779f45258184e6a
sglim <seung.gi.lim@gmail.com>
2018-03-03 20:59:23 +0900

    Apply crawler default example
    - Current editor is VIM

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Crawler');
+// Sample
+// https://www.rocketpunch.com/companies?page=1&q=
+
+var Crawler = require("crawler");
+
+function main() {
+  var c = new Crawler({
+      maxConnections : 10,
+      // This will be called for each crawled page
+      callback : function (error, res, done) {
+          if(error) {
+              console.log(error);
+          } else {
+              var $ = res.$;
+              // $ is Cheerio by default
+              //a lean implementation of core jQuery designed specifically for the server
+              console.log($("title").text());
+          }
+          done();
+      }
+  });
+
+  c.queue('https://www.rocketpunch.com/companies?page=1&q=');
+}
+
+if (require.main === module) {
+    main();
+}
+
