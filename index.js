8cd995e35202cbf9a52325a742c07a649cad926e
sglim <seung.gi.lim@gmail.com>
2018-03-03 21:27:23 +0900

    Crawl page 1 correctly

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-// Sample
-// https://www.rocketpunch.com/companies?page=1&q=
+'use strict';
 
-var Crawler = require("crawler");
+// Sample
+// No! https://www.rocketpunch.com/companies?page=1&q=
+// Yes! https://www.rocketpunch.com/api/companies/template?page=1&q=
+
+
+const _ = require('lodash');
+const cheerio = require('cheerio')
+const Crawler = require("crawler");
 
 function main() {
-  var c = new Crawler({
+  const c = new Crawler({
+      jquery: false,
       maxConnections : 10,
       // This will be called for each crawled page
       callback : function (error, res, done) {
           if(error) {
               console.log(error);
           } else {
-              var $ = res.$;
-              // $ is Cheerio by default
-              //a lean implementation of core jQuery designed specifically for the server
-              console.log($("title").text());
+              const parsed = JSON.parse(res.body);
+              const template = parsed.data.template;
+              const $ = cheerio.load(template)
+              $('#company-list .company').each((index, item) => {
+                  const name = $(item).find('.name strong').text().trim();
+                  const metadata = $(item).find('.meta').text().trim();
+                  console.log(_.chain(metadata).split(' ∙ ').value());
+                  const metadataList = metadata.split(' ∙ ');
+                  // console.log(name);
+                  // console.log(metadata);
+              });
           }
           done();
       }
   });
 
-  c.queue('https://www.rocketpunch.com/companies?page=1&q=');
+  c.queue('https://www.rocketpunch.com/api/companies/template?page=1&q=');
 }
 
 if (require.main === module) {
