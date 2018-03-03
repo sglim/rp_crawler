47c61847ce1188f5d2ab78293fcfcf7822bc8ff4
sglim <seung.gi.lim@gmail.com>
2018-03-03 21:52:53 +0900

    Save into csv and add get page crawler

--- index.js
+++ index.js
@@ -8,36 +8,73 @@
 const _ = require('lodash');
 const cheerio = require('cheerio')
 const Crawler = require("crawler");
+const csvWriter = require('csv-write-stream');
+const fs = require('fs');
+
+function parseTemplateBody(body) {
+  const parsed = JSON.parse(body);
+  const template = parsed.data.template;
+  return cheerio.load(template)
+}
+
+function getPage() {
+  const c = new Crawler({
+    jquery: false,
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+      if(error) {
+        console.log(error);
+      } else {
+        const $ = parseTemplateBody(res.body);
+        const lastPage = $('.pagination .computer .item:last-child').text();
+        console.log(parseInt(lastPage.replace(',', ''), 10));
+        // TODO(sglim): Crawl page here.
+      }
+    }
+  });
+  c.queue('https://www.rocketpunch.com/api/companies/template');
+}
 
 function main() {
+  getPage();
+}
+
+function crawl(page) {
+  const writer = csvWriter();
+  writer.pipe(fs.createWriteStream('out.csv'))
+
   const c = new Crawler({
-      jquery: false,
-      maxConnections : 10,
-      // This will be called for each crawled page
-      callback : function (error, res, done) {
-          if(error) {
-              console.log(error);
-          } else {
-              const parsed = JSON.parse(res.body);
-              const template = parsed.data.template;
-              const $ = cheerio.load(template)
-              $('#company-list .company').each((index, item) => {
-                  const name = $(item).find('.name strong').text().trim();
-                  const metadata = $(item).find('.meta').text().trim();
-                  console.log(_.chain(metadata).split(' ∙ ').value());
-                  const metadataList = metadata.split(' ∙ ');
-                  // console.log(name);
-                  // console.log(metadata);
-              });
-          }
-          done();
+    jquery: false,
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+      if(error) {
+        console.log(error);
+      } else {
+        const parsed = JSON.parse(res.body);
+        const template = parsed.data.template;
+        const $ = cheerio.load(template)
+        $('#company-list .company').each((index, item) => {
+          const name = $(item).find('.name strong').text().trim();
+          const metadata = $(item).find('.meta').text().trim();
+          const metadataList = metadata.split(' ∙ ');
+          writer.write({
+            'name': name,
+            'metadata': metadataList.join(', '),
+          });
+        });
       }
+      // TODO(sglim): Finish iff all data received
+      writer.end()
+      done();
+    }
   });
 
-  c.queue('https://www.rocketpunch.com/api/companies/template?page=1&q=');
+  c.queue(`https://www.rocketpunch.com/api/companies/template?page=${page}&q=`);
 }
 
 if (require.main === module) {
-    main();
+  main();
 }
 
